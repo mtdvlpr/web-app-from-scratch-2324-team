cb8f0ca8696dab6669ebe5ba4d6c0fff6b91c4eb
yelunl <yeliz.unlu@hva.nl>
2024-02-13 10:24:11 +0100

    add html

--- src/assets/js/team.js
+++ src/assets/js/team.js
@@ -8,17 +8,17 @@
  * }} info
  */
 export const setTeamInfo = (info) => {
-  const title = document.getElementById("title");
-  title.textContent = info.teamName;
+  // const title = document.getElementById("title");
+  // title.textContent = info.teamName;
 
-  const list = document.createElement("ul");
-  info.members.forEach((member) => {
-    const listItem = document.createElement("li");
-    listItem.textContent = member.name;
-    list.appendChild(listItem);
-  });
+  // const list = document.createElement("ul");
+  // info.members.forEach((member) => {
+  //   const listItem = document.createElement("li");
+  //   listItem.textContent = member.name;
+  //   list.appendChild(listItem);
+  // });
 
-  const container = document.getElementById("member-container");
-  container.innerHTML = "";
-  container.appendChild(list);
+  // const container = document.getElementById("member-container");
+  // container.innerHTML = "";
+  // container.appendChild(list);
 };
