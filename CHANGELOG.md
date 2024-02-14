595da4aff6ef9cef89140470bde0193215232642
Eva Boogaard <evaboogaard@hotmail.com>
2024-02-14 13:10:17 +0100

    commit

---
+++ CHANGELOG.md
@@ -0,0 +1,14 @@
+# Changelog
+
+## 12-02-2024
+
+- First setup of the application (hello world)
+- Created a [design in Figma](https://www.figma.com/file/J0LWpqkhzN6299S9d0ONdG/WAFS-team-design?type=design&node-id=0%3A1&mode=design&t=AeRfs7XoigEnfK3Y-1)
+
+## 13-02-2024
+
+- Added the base HTML structure and some styling with CSS
+
+## 14-02-2024
+
+- Added the JavaScript logic for fetching data and navigation
---
+++ src/assets/js/api.js
@@ -0,0 +1,83 @@
+"use strict";
+
+/**
+ * Fetches local team data from ./team.json
+ * @returns {Promise<{
+ * teamName: string
+ * members: {name: string; personalPage: string}[]
+ * }>} The fetched data
+ */
+export async function fetchTeamData() {
+  try {
+    const response = await fetch("./team.json");
+    return await response.json();
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+/**
+ * Fetches personal data from a team member
+ * @param {string} personalPage The URL of the personal page of the member
+ * @returns {Promise<{
+ * firstName: string
+ * lastName: string
+ * avatar_url: string
+ * age: number
+ * bio: string
+ * stats: {title: string; value: number}[]
+ * strengths: string[]
+ * weaknesses: string[]
+ * habitat: {title: string; value: number}[]
+ * favoritePokemon: string
+ * favoritePokemonAvatar: string
+ * favoritePokemonColor: string
+ * }>} The fetched data
+ */
+export const fetchPersonData = async (personalPage) => {
+  try {
+    const response = await fetch(
+      `${personalPage}${personalPage.endsWith("/") ? "" : "/"}info.json`
+    );
+    return response.json();
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+/**
+ * Fetches personal data from each team member
+ * @param {{
+ * teamName: string
+ * members: {name: string; personalPage: string}[]
+ * }} team The team object
+ * @returns {Promise<{
+ * firstName: string
+ * lastName: string
+ * avatar_url: string
+ * age: number
+ * bio: string
+ * stats: {title: string; value: number}[]
+ * strengths: string[]
+ * weaknesses: string[]
+ * habitat: {title: string; value: number}[]
+ * favoritePokemon: string
+ * favoritePokemonAvatar: string
+ * favoritePokemonColor: string
+ * }[]>} The team members data
+ */
+export const fetchTeamMembers = async (team) => {
+  const promises = [];
+  team.members.forEach((member) => {
+    promises.push(fetchPersonData(member.personalPage));
+  });
+
+  const members = [];
+  const result = await Promise.allSettled(promises);
+  result.forEach((memberResult) => {
+    if (memberResult.status === "fulfilled") {
+      members.push(memberResult.value);
+    }
+  });
+  return members;
+};
