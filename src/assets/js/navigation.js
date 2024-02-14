875639bbf7ffcbd2f16eae81b4c8f0c3907e5c53
Manoah Tervoort <149895ja@gmail.com>
2024-02-14 13:18:23 +0100

    chore: prettier

--- src/assets/js/navigation.js
+++ src/assets/js/navigation.js
@@ -1,9 +1,9 @@
-"use strict";
+'use strict'
 
-import { setActiveMember } from "member";
+import { setActiveMember } from 'member'
 
 // Constants
-const DETAIL_ARTICLES = ["about", "stats", "habitats"];
+const DETAIL_ARTICLES = ['about', 'stats', 'habitats']
 
 /**
  * Fills the main navigation with members
@@ -23,113 +23,113 @@ const DETAIL_ARTICLES = ["about", "stats", "habitats"];
  * }[]} members The members to load
  */
 export const initNavigation = (members) => {
-  const list = document.createElement("ul");
+  const list = document.createElement('ul')
 
   members.forEach((member) => {
-    const listItem = document.createElement("li");
+    const listItem = document.createElement('li')
 
-    const link = document.createElement("a");
-    link.href = `#/${member.firstName}/about`;
+    const link = document.createElement('a')
+    link.href = `#/${member.firstName}/about`
     link.onclick = () => {
-      setActiveMember(member);
-      setActiveTab("about");
-    };
+      setActiveMember(member)
+      setActiveTab('about')
+    }
 
-    const img = document.createElement("img");
-    img.src = member.favoritePokemonAvatar;
-    img.alt = member.firstName;
+    const img = document.createElement('img')
+    img.src = member.favoritePokemonAvatar
+    img.alt = member.firstName
 
-    link.appendChild(img);
-    listItem.appendChild(link);
-    list.appendChild(listItem);
-  });
+    link.appendChild(img)
+    listItem.appendChild(link)
+    list.appendChild(listItem)
+  })
 
-  const container = document.getElementById("nav-main");
-  container.innerHTML = "";
-  container.appendChild(list);
-};
+  const container = document.getElementById('nav-main')
+  container.innerHTML = ''
+  container.appendChild(list)
+}
 
 /**
  * Sets the active page
  * @param {string | undefined} page The page to display
  */
 export const setActivePage = (page) => {
-  setURL({ page });
+  setURL({ page })
 
   // Set active state for navigation items
-  const navItems = document.querySelectorAll("#nav-main a");
+  const navItems = document.querySelectorAll('#nav-main a')
   navItems.forEach((navItem) => {
     navItem.classList.toggle(
-      "active",
-      navItem.getAttribute("href").startsWith(`#/${page}`)
-    );
-  });
+      'active',
+      navItem.getAttribute('href').startsWith(`#/${page}`)
+    )
+  })
 
   // Update tab links
-  const tabs = document.querySelectorAll("#nav-sub a");
+  const tabs = document.querySelectorAll('#nav-sub a')
   tabs.forEach((tab) => {
-    tab.href = `#/${page}/${tab.textContent.toLowerCase().trim()}`;
-  });
+    tab.href = `#/${page}/${tab.textContent.toLowerCase().trim()}`
+  })
 
   // Show/hide team article and tabs
-  const teamContainer = document.getElementById("team");
-  teamContainer.classList.toggle("active", !page);
-  const subNav = document.getElementById("nav-sub");
-  subNav.classList.toggle("active", !!page);
-  if (!page) setActiveTab("");
-};
+  const teamContainer = document.getElementById('team')
+  teamContainer.classList.toggle('active', !page)
+  const subNav = document.getElementById('nav-sub')
+  subNav.classList.toggle('active', !!page)
+  if (!page) setActiveTab('')
+}
 
 /**
  * Initializes the click events for the tabs
  */
 export const initTabs = () => {
-  const tabs = document.querySelectorAll("#nav-sub a");
+  const tabs = document.querySelectorAll('#nav-sub a')
   tabs.forEach((tab) => {
-    tab.onclick = () => setActiveTab(tab.textContent.toLowerCase().trim());
-  });
-};
+    tab.onclick = () => setActiveTab(tab.textContent.toLowerCase().trim())
+  })
+}
 
 /**
  * Sets the active tab
  * @param {string} tab The tab to activate
  */
 export const setActiveTab = (tab) => {
-  setURL({ tab });
+  setURL({ tab })
 
   // Set active state for tab links
-  const tabs = document.querySelectorAll("#nav-sub a");
+  const tabs = document.querySelectorAll('#nav-sub a')
   tabs.forEach((tabEl) => {
     tabEl.classList.toggle(
-      "active",
+      'active',
       tabEl.textContent.toLowerCase().trim() === tab
-    );
-  });
+    )
+  })
 
   // Show active article
   DETAIL_ARTICLES.forEach((article) => {
-    const element = document.getElementById(article);
-    element.classList.toggle("active", article === tab);
-  });
-};
+    const element = document.getElementById(article)
+    element.classList.toggle('active', article === tab)
+  })
+}
 
 /**
  * Sets the current URL
  * @param {{page?: string; tab?: string; home?: boolean}} param0 The page and tab to set
  */
 export const setURL = ({ page, tab } = {}) => {
-  const url = new URL(window.location);
-  const [currentPage, currentTab] = url.hash.substring(2).split("/");
-  url.hash = page || tab ? `#/${page ?? currentPage}/${tab ?? currentTab}` : "";
-  window.history.pushState(null, "", url.toString());
-};
+  const url = new URL(window.location)
+  const [currentPage, currentTab] = url.hash.substring(2).split('/')
+  url.hash = page || tab ? `#/${page ?? currentPage}/${tab ?? currentTab}` : ''
+  window.history.pushState(null, '', url.toString())
+}
 
 /**
  * Sets the navigation to loading state
  */
 export const setNavigationLoading = () => {
-  const navItems = document.querySelectorAll("#nav-main a");
-  const pokeballTemplate = document.getElementById("pokeball-loader");
+  const navItems = document.querySelectorAll('#nav-main a')
+  const pokeballTemplate = document.getElementById('pokeball-loader')
   navItems.forEach((navItem) => {
-    navItem.innerHTML = pokeballTemplate.innerHTML;
-  });
-};
+    navItem.innerHTML = pokeballTemplate.innerHTML
+  })
+}
--- src/main.js
+++ src/main.js
@@ -1,36 +1,36 @@
-"use strict";
+'use strict'
 
 // Main entry point of the app
-import { fetchTeamData, fetchTeamMembers } from "api";
-import { setActiveMember } from "member";
+import { fetchTeamData, fetchTeamMembers } from 'api'
+import { setActiveMember } from 'member'
 import {
   initTabs,
   initNavigation,
   setActiveTab,
   setNavigationLoading,
-} from "navigation";
+} from 'navigation'
 
 // Loads data in the app
 async function loadData() {
-  setNavigationLoading();
-  const team = await fetchTeamData();
-  const members = await fetchTeamMembers(team);
-  initNavigation(members);
+  setNavigationLoading()
+  const team = await fetchTeamData()
+  const members = await fetchTeamMembers(team)
+  initNavigation(members)
 
-  const url = new URL(window.location);
+  const url = new URL(window.location)
   const member = members.find((member) =>
     url.hash.startsWith(`#/${member.firstName}`)
-  );
+  )
   if (member) {
-    setActiveMember(member);
+    setActiveMember(member)
     setActiveTab(
-      url.hash.split("/").pop()?.replace(member.firstName, "") || "about"
-    );
+      url.hash.split('/').pop()?.replace(member.firstName, '') || 'about'
+    )
   } else {
-    url.hash = "";
-    window.history.pushState(null, "", url.toString());
+    url.hash = ''
+    window.history.pushState(null, '', url.toString())
   }
 }
 
-initTabs();
-loadData();
+initTabs()
+loadData()
